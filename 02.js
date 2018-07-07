96c3a2a51c8861d85a936a043dfb26e968b96e87
Edi <40353224+thejuniordevops@users.noreply.github.com>
2018-07-07 16:22:22 +0700

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,26 @@
+/*
+A program where you can combine several 
+arrays into one array 
+
+
+*/
+
+const first = ['Behind', 'every', 'great', 'man'];
+const second = ['is', 'a','woman'];
+const third = ['rolling', 'her', 'eyes']; 
+
+//create a function using the arrow function 
+let combineAll = (firstArray, secondArray, thirdArray) => {
+
+    let newArray = firstArray.concat(secondArray, thirdArray); 
+
+    let newString = newArray.join(' ');
+
+    return newString; 
+
+
+
+}; 
+
+//execute the function and print the new value  
+console.log(combineAll(first, second, third)); 
